0382e5c70ae76a256d0156e706736456153546a0
Wen Li <wen.li@ucl.ac.uk>
2015-08-24 03:27:37 +0100

    Fix typo

--- twitter_proxy.py
+++ twitter_proxy.py
@@ -27,7 +27,7 @@ with open('cred.json') as fin:
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 
-@app.rout('/')
+@app.route('/')
 def index():
     return redirect('/examples/example.html')
 
