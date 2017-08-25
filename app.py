bece0ae200fe4d76ef9c7f67ff98aae765618174
Wen Li <wen.li@ucl.ac.uk>
2017-08-25 23:25:13 +0100

    Remove vertenv for v3

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import sys
 #hack to make sure we can load wsgi.py as a module in this class
 sys.path.insert(0, os.path.dirname(__file__))
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    print >>sys.sstderr, 'Failed to get into virtualenv'
+# virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+# virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+# try:
+#     execfile(virtualenv, dict(__file__=virtualenv))
+# except IOError:
+#     print >>sys.sstderr, 'Failed to get into virtualenv'
 #
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
